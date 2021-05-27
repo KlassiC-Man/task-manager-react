479086df2f35318ba539659a7c96e57a017d2ddd
KlassiC-Man <parth.11.gulati@gmail.com>
2021-05-27 20:52:01 +0530

    Still Incomplete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -22,11 +23,31 @@ function App() {
         day: "May 26th at 12:00pm",
         reminder: false,
     }
-    ])
+  ])
+
+  //addTask
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    console.log(id);
+    const newTask = {id, ...task};
+    setTasks([...tasks, newTask]);
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
+  //Task Deleter!
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }  
+
   return (
     <div className="container">
+      <AddTask onAdd={addTask} />
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks Pending!'}
     </div>
   );
 }
